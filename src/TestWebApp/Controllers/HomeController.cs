b648c83a66bbb7543d013e30e3c9f16f01770b62
Björn Lundström <viper78@gmx.net>
2018-02-11 21:04:37 +0100

    add test web projekt

---
+++ src/TestWebApp/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using BLun.SuitOfArmor.Common.CacheControle;
+using BLun.SuitOfArmor.Filter;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using TestWebApp.Models;
+
+namespace TestWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [XssProtection(XssProtection.EnablesForOldIe8)]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        [CacheControl(CacheResponseDirective.NoCache)]
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
